9a330c359ff2f7255760821ede9caeec20f4a5f3
Monroe Gollaher <mgollaher92@gmail.com>
2021-02-12 17:15:36 -0700

    changed how axios / actions are setup

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,10 +1,11 @@
-import * as api from '../api/axios'
+import { api } from '../api/axios'
 
 export const getPosts = () => async (dispatch) => {
 
   try {
-      const { data } = await api.fetchPosts()
+      const { data } = await api.get('/posts')
       dispatch({ type: 'FETCH-ALL', payload: data })
+      console.log(data, 'test');
   } catch (error) {
       console.log(error.message);
   }
--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
-const url = window.location.origin.includes('localhost') ? 'http://localhost:5000/posts' : ''
+const baseURL = window.location.origin.includes('localhost') ? 'http://localhost:5000' : ''
 
-export const fetchPosts = () => axios.get(url)
-// export const api = axios.create({
-//   baseURL,
-//   timeout: 8000
-// })
+export const api = axios.create({
+  baseURL,
+  timeout: 8000
+})
