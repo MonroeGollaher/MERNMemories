573b8852e4bcc851964cf284dc6764a2bcfd975c
Monroe Gollaher <mgollaher92@gmail.com>
2021-02-18 13:09:38 -0700

    dotenv

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config()
 
 // @ts-ignore
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -14,10 +16,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://testUser:Passw0rd!@cluster0.gpg4p.mongodb.net/Mern?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
